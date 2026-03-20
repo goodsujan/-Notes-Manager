7283d8cd8bd363997ad4c3459efa2af6c4c627cb
goodsujan <ethical.suzan@gmail.com>
2026-03-20 18:22:03 +0545

    feat: implement list view with pagination and search

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Note
+from .forms import NoteForm
+from django.core.paginator import Paginator
 
-# Create your views here.
+def note_create(request):
+    form = NoteForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect('notes:list')
+    return render(request, 'notes/create.html', {'form': form})
