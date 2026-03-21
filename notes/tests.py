2e2d72723724e447537711e31f6f2aca225b16c9
goodsujan <ethical.suzan@gmail.com>
2026-03-21 12:29:20 +0545

    add basic tests for model and views

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Note
 
-# Create your tests here.
+class NoteModelTest(TestCase):
+    def test_create_note(self):
+        n = Note.objects.create(title="Test", content="abc")
+        self.assertEqual(Note.objects.count(), 1)
+        self.assertEqual(n.title, "Test")
+
+class NoteViewTests(TestCase):
+    def test_list_view(self):
+        Note.objects.create(title="A", content="one")
+        resp = self.client.get(reverse('notes:list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, "A")
